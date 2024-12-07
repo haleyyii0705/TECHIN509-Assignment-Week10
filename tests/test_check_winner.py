153e355e34aa78ddfbf2ca4b9ca4780e15853e72
luyao <luyao@uw.edu>
2024-12-07 00:45:47 -0800

    Add and fix unit tests for check_winner method

--- tests/test_check_winner.py
+++ tests/test_check_winner.py
@@ -4,12 +4,27 @@ from models.board import Board
 class TestCheckWinner(unittest.TestCase):
     def test_winner_in_row(self):
         board = Board()
-        board.board = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
+        board.grid = [["X", "X", "X"], [" ", " ", " "], [" ", " ", " "]]
+        self.assertEqual(board.check_winner(), "X")
+
+    def test_winner_in_column(self):
+        board = Board()
+        board.grid = [["X", " ", " "], ["X", " ", " "], ["X", " ", " "]]
+        self.assertEqual(board.check_winner(), "X")
+
+    def test_winner_in_diagonal(self):
+        board = Board()
+        board.grid = [["X", " ", " "], [" ", "X", " "], [" ", " ", "X"]]
         self.assertEqual(board.check_winner(), "X")
 
     def test_no_winner(self):
         board = Board()
-        board.board = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
+        board.grid = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
+        self.assertIsNone(board.check_winner())
+
+    def test_empty_board(self):
+        board = Board()
+        board.grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
         self.assertIsNone(board.check_winner())
 
 if __name__ == "__main__":
