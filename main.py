f5279e90ecc11ef84af5e618d110a148d2872a5e
luyao <luyao@uw.edu>
2024-12-07 00:13:04 -0800

    Add unit tests for check_winner method

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from utils.game_logic import play_tic_tac_toe
 
+def play_tic_tac_toe():
+    board = Board()
+    board.draw_board()
+
 if __name__ == "__main__":
     play_tic_tac_toe()
